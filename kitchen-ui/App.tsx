e99bd17c52817379b2735ceafee9f52df670498b
kahgeh.tan <kahgeh.tan@xero.com>
2020-06-27 14:08:28 +1000

    create basic menu component

--- kitchen-ui/App.tsx
+++ kitchen-ui/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import Menu from "./Menu";
 
 const App = () => {
     return (<div className="container">
         <div className="header"></div>
-        <div className="body"></div>
+        <div className="body">
+            <Menu />
+        </div>
         <div className="footer"></div>
     </div>
     )
