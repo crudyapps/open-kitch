53c57771f78625a60ee4317fd5c9ac412b8e1aa0
kahgeh.tan <kahgeh.tan@xero.com>
2020-06-30 10:29:27 +1000

    move ui code into src

--- kitch-ui/src/App.tsx
+++ kitch-ui/src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
-import { Route, BrowserRouter as Router, } from "react-router-dom";
+import { Route, BrowserRouter, } from "react-router-dom";
 import MenuItemsPage from "./MenuItemsPage";
 import OrderPage from "./OrderPage";
 
 const App = () => {
     return (
-        <Router>
+        <BrowserRouter>
             <Route exact path="/" component={MenuItemsPage} />
             <Route exact path="/menuItems" component={MenuItemsPage} />
             <Route exact path="/orders" component={OrderPage} />
             <Route exact path="/orders/:orderId" component={OrderPage} />
-        </Router>
+        </BrowserRouter>
     )
 }
 
